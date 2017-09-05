9c45421736cdc1554b399c55d42d097e5965dd19
craig-mulligan <craig@resin.io>
2017-09-05 13:19:36 -0400

    Add Banner Component
    Connects to #32
    
    Change-Type: patch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { default as Button } from './components/Button'
 import { default as Provider } from './components/Provider'
 import { default as DeviceStatusGauge } from './components/DeviceStatusGauge'
-
+import { default as Banner } from './components/Banner'
 import { Flex, Box } from './components/Grid'
 
-export { Box, Button, Flex, Provider, DeviceStatusGauge }
+export { Banner, Box, Button, Flex, Provider, DeviceStatusGauge }
---
+++ src/stories/Banner.js
@@ -0,0 +1,14 @@
+import { h } from 'preact'
+import { storiesOf } from '@storybook/react'
+import { Banner } from '../'
+import bgImage from './assets/bg.png'
+
+storiesOf('Banner', module)
+  .addWithInfo('BackgoundImage', () => {
+    return (
+      <Banner color='white' backgroundImage={bgImage}>
+        <h1>Resin.io</h1>
+        <p>Resin.io brings the benefits of Linux containers to the IoT. Develop iteratively, deploy safely, and manage at scale.</p>
+      </Banner>
+    )
+  })
