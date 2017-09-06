34fb0c6bf12336f3b399434fd6cfede9519677f0
Lucian Buzzo <lucian.buzzo@gmail.com>
2017-09-06 08:53:58 +0100

    use px() helper

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import Button from './Button'
 import Fixed from './Fixed'
 import Text from './Text'
+import { px } from '../utils'
 import { Flex } from './Grid'
 
 const MODAL_WIDTH = 700
@@ -12,11 +13,11 @@ const ModalPanel = styled.div`
   background-color: #ffffff;
   border: solid 0.5px #9b9b9b;
   position: fixed;
-  width: ${MODAL_WIDTH}px;
+  width: ${px(MODAL_WIDTH)};
   top: 225px;
   min-height: 50px;
   left: 50%;
-  margin-left: -${MODAL_WIDTH / 2}px;
+  margin-left: -${px(MODAL_WIDTH / 2)};
   padding: 30px 50px;
   z-index: 10;
   box-shadow: 0px 0px 60px 1px rgba(0, 0, 0, 0.4);
