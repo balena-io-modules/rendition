95ce6b3c664995a7dcdb5fb136d0cffc8b3d2670
Thodoris Greasidis <thgreasi@gmail.com>
2017-09-25 13:06:39 +0300

    Fix gauge color when only one item has value
    Connects-To: #69
    Change-Type: patch

--- src/stories/Gauge.js
+++ src/stories/Gauge.js
@@ -20,6 +20,28 @@ const data = [
   }
 ]
 
-storiesOf('Gauge', module).addWithInfo('default', () => {
-  return <Gauge m={30} title='Ice Cream' data={data} />
-})
+const singleItem = [
+  {
+    value: 0,
+    name: 'Vanilla',
+    color: 'beige'
+  },
+  {
+    value: 0,
+    name: 'Chocolate',
+    color: 'brown'
+  },
+  {
+    value: 2,
+    name: 'Strawberry',
+    color: 'pink'
+  }
+]
+
+storiesOf('Gauge', module)
+  .addWithInfo('default', () => {
+    return <Gauge m={30} title='Ice Cream' data={data} />
+  })
+  .addWithInfo('single segment', () => {
+    return <Gauge m={30} title='Ice Cream' data={singleItem} />
+  })
