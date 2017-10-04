0c7c35edf25b9047553058b4d6dd00c6cad2f3e3
craig-mulligan <craig@resin.io>
2017-10-04 11:45:36 +0100

    Add Container component
    Connects to #99
    
    Change-Type: minor

---
+++ src/components/Container.js
@@ -0,0 +1,31 @@
+import { h } from 'preact'
+import styled, { withTheme } from 'styled-components'
+import {
+  fontSize,
+  space,
+  color,
+  textAlign,
+  responsiveStyle
+} from 'styled-system'
+
+const maxWidth = responsiveStyle('max-width', 'maxWidth')
+
+const Container = styled.div`
+  ${textAlign} ${maxWidth} ${fontSize} ${space} ${color};
+`
+
+Container.displayName = 'Container'
+Container.defaultProps = {
+  px: 3,
+  ml: 'auto',
+  mr: 'auto'
+}
+
+export default withTheme(({ theme, ...props }) => {
+  return (
+    <Container
+      {...props}
+      maxWidth={theme.breakpoints.map((bp, i) => `${bp - theme.space[i]}em`)}
+    />
+  )
+})
