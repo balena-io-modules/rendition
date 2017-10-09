3ab25ad0277db750deaa451610364ad6324a0889
Lucian Buzzo <lucian.buzzo@gmail.com>
2017-10-09 11:50:44 +0100

    Add `disabled` attribute to Link component
    Fixes #135
    
    change-type: minor

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,23 +1,29 @@
 import { h } from 'preact'
 import Text from './Text'
 import get from 'lodash/get'
+import omit from 'lodash/omit'
 import { darken } from '../utils'
 
 let Base = Text.extend`
   text-decoration: ${props => props.decor || 'none'};
-  cursor: pointer;
+  cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
+  opacity: ${props => (props.disabled ? 0.65 : 1)};
   display: inline-block;
 
   &:active,
   &:hover {
     color: ${props =>
-    darken(get(props.theme.colors, props.color) || props.color)};
+    !props.disabled &&
+      darken(get(props.theme.colors, props.color) || props.color)};
   }
 `
 
 const Link = ({ is, blank, ...props }) => {
   if (is) {
     Base = Base.withComponent(is)
+  }
+  if (props.disabled) {
+    props = omit(props, 'href')
   }
   return (
     <Base {...props} rel={blank && 'noopener'} target={blank && '_blank'} />
