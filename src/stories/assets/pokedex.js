713bac2302cdd33506aa55465824c9b456280799
Lucian Buzzo <lucian.buzzo@gmail.com>
2017-10-30 15:38:22 +0000

    Don't show an input field when filtering on a boolean value
    Fixes #80
    
    change-type: patch

--- src/stories/assets/pokedex.js
+++ src/stories/assets/pokedex.js
@@ -7,7 +7,8 @@ export default [
       "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.",
     Category: 'Seed',
     Abilities: 'Overgrow',
-    pokedex_number: 1
+    pokedex_number: 1,
+    caught: true
   },
   {
     Name: 'Ivysaur',
@@ -17,7 +18,8 @@ export default [
       "There is a bud on this Pokemon's back. To support its weight, Ivysaur's legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it's a sign that the bud will bloom into a large flower soon.",
     Category: 'Seed',
     Abilities: 'Overgrow',
-    pokedex_number: 2
+    pokedex_number: 2,
+    caught: false
   },
   {
     Name: 'Venusaur',
@@ -27,7 +29,8 @@ export default [
       "There is a large flower on Venusaur's back. The flower is said to take on vivid colors if it gets plenty of nutrition and sunlight. The flower's aroma soothes the emotions of people.",
     Category: 'Seed',
     Abilities: 'Overgrow',
-    pokedex_numberr: 3
+    pokedex_number: 3,
+    caught: false
   },
   {
     Name: 'Charmander',
@@ -37,7 +40,8 @@ export default [
       'The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself. If the Pokemon becomes enraged, the flame burns fiercely.',
     Category: 'Lizard',
     Abilities: 'Blaze',
-    pokedex_number: 4
+    pokedex_number: 4,
+    caught: false
   },
   {
     Name: 'Charmeleon',
@@ -48,7 +52,8 @@ export default [
     Category: 'Flame',
     Abilities: 'Blaze',
     pokedex_number: 5,
-    first_seen: '2012-01-01T14:43:02.000Z'
+    first_seen: '2012-01-01T14:43:02.000Z',
+    caught: false
   },
   {
     Name: 'Charizard',
@@ -58,7 +63,8 @@ export default [
       'Charizard flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponent weaker than itself.',
     Category: 'Flame',
     Abilities: 'Blaze',
-    pokedex_number: 6
+    pokedex_number: 6,
+    caught: false
   },
   {
     Name: 'Squirtle',
@@ -69,7 +75,8 @@ export default [
     Category: 'Tiny Turtle',
     Abilities: 'Torrent',
     pokedex_number: 7,
-    first_seen: '2017-01-01T08:49:26.961Z'
+    first_seen: '2017-01-01T08:49:26.961Z',
+    caught: false
   },
   {
     Name: 'Wartortle',
@@ -79,7 +86,8 @@ export default [
       "Its tail is large and covered with a rich, thick fur. The tail becomes increasingly deeper in color as Wartortle ages. The scratches on its shell are evidence of this Pokemon's toughness as a battler.",
     Category: 'Turtle',
     Abilities: 'Torrent',
-    pokedex_number: 8
+    pokedex_number: 8,
+    caught: false
   },
   {
     Name: 'Blastoise',
@@ -89,7 +97,8 @@ export default [
       'Blastoise has water spouts that protrude from its shell. The water spouts are very accurate. They can shoot bullets of water with enough accuracy to strike empty cans from a distance of over 160 feet.',
     Category: 'Shellfish',
     Abilities: 'Torrent',
-    pokedex_number: 9
+    pokedex_number: 9,
+    caught: false
   },
   {
     Name: 'Caterpie',
@@ -99,6 +108,7 @@ export default [
       'Caterpie has a voracious appetite. It can devour leaves bigger than its body right before your eyes. From its antenna, this Pokémon releases a terrifically strong odor.',
     Category: 'Worm',
     Abilities: 'Shield Dust',
-    pokedex_number: 10
+    pokedex_number: 10,
+    caught: true
   }
 ]
