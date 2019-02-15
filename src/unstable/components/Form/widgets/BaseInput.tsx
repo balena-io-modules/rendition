b3dc9d0ac9fa6fd57ff7409edc5990649245fa3e
Lucian <lucian.buzzo@gmail.com>
2019-02-15 15:25:55 +0000

    Form: Improve inputs for numbers and integers
    Change-type: patch
    Signed-off-by: Lucian <lucian.buzzo@gmail.com>

--- src/unstable/components/Form/widgets/BaseInput.tsx
+++ src/unstable/components/Form/widgets/BaseInput.tsx
@@ -18,10 +18,30 @@ const BaseInput = (props: FormWidgetProps) => {
 		...inputProps
 	} = props;
 
-	if (schema.type === 'number') {
-		inputProps.type = 'number';
+	// If options.inputType is set use that as the input type
+	if (options.inputType) {
+		inputProps.type = options.inputType;
 	} else {
-		inputProps.type = options.inputType || inputProps.type || 'text';
+		// If the schema is of type number or integer, set the input type to number
+		if (schema.type === 'number') {
+			inputProps.type = 'number';
+			// Setting step to 'any' fixes a bug in Safari where decimals are not
+			// allowed in number inputs
+			(inputProps as any).step = 'any';
+		} else if (schema.type === 'integer') {
+			inputProps.type = 'number';
+			// Since this is integer, you always want to step up or down in multiples
+			// of 1
+			(inputProps as any).step = '1';
+		} else {
+			inputProps.type = 'text';
+		}
+	}
+
+	// If multipleOf is defined, use this as the step value. This mainly improves
+	// the experience for keyboard users (who can use the up/down KB arrows).
+	if (schema.multipleOf) {
+		(inputProps as any).step = schema.multipleOf;
 	}
 
 	const change = ({ target: { value } }: any) => {
