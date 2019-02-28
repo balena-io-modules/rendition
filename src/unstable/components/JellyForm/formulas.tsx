2d1d36c0cb4f7678410d167b62c89be6268ef05a
Cyryl Płotnicki <cyryl@balena.io>
2019-02-28 10:28:11 +0000

    Fixes #759 - a problem where if a formula definition would be inside of an array, it would not get evaluated properly.
    More info:
    We preprocess the data before sending it to temen to evaluate - we put the formulas from the schema in the relevant places inside the data. This was done for objects but not for arrays of objects. The code in this PR basically goes through the data object and makes sure that for every item in every array on any level we invoke the substitution of existing data for the formula.
    
    In the future - it may be good to move all this `data + schema => data_with_formulas` logic inside reconfix maybe, so that JellyForm only calls these functions ? Or maybe there is an existing functionality that we could reuse here ?
    
    Change-type: patch

--- src/unstable/components/JellyForm/formulas.tsx
+++ src/unstable/components/JellyForm/formulas.tsx
@@ -6,43 +6,12 @@ import isPlainObject = require('lodash/isPlainObject');
 import merge = require('lodash/merge');
 import set = require('lodash/set');
 
-const getFormulaKeys = function(obj: any, prefix: string = '') {
-	const keys = Object.keys(obj);
-	prefix = prefix ? prefix + '.' : '';
-	return keys.reduce(
-		(result, key) => {
-			if (isPlainObject(obj[key])) {
-				result = result.concat(getFormulaKeys(obj[key], prefix + key));
-			} else if (key === '$$formula') {
-				result.push(prefix + key);
-			}
-			return result;
-		},
-		[] as string[],
-	);
-};
-
 // Given a JSON Schema, find all fields that have a formula field, evaluate the
 // formula and then update the provided value with the result
 const runFormulas = (schema: JSONSchema6, value: any) => {
-	// Start by cloning the value to avoid any awkward mutation bugs
 	const data = cloneDeep(value);
 
-	// Find all the keypaths that use formulas
-	const keys = getFormulaKeys(schema).map(path => {
-		return {
-			formula: get(schema, path),
-			path: path.replace(/properties\./g, ''),
-		};
-	});
-
-	// Replace each field that is the result of a formula with the formula itself.
-	// This is done because the formula may reference other fields in the data
-	// object, and they all need to be present when running the object through
-	// temen.
-	keys.forEach(y => {
-		set(data, y.path, y.formula);
-	});
+	replaceDataWithFormulaContent(schema, data);
 
 	let evaluate;
 
@@ -60,6 +29,68 @@ const runFormulas = (schema: JSONSchema6, value: any) => {
 
 const defaultValueForSchema = (schema: JSONSchema6) => {
 	return schema.type === 'array' ? [] : {};
+};
+
+const replaceDataWithFormulaContent = (schema: JSONSchema6, data: any) => {
+	// Find all the keypaths that use formulas
+	const keys = getFormulaKeys(schema).map(path => {
+		return {
+			formula: get(schema, path),
+			path: path.replace(/properties\./g, ''),
+		};
+	});
+
+	// Replace each field that is the result of a formula with the formula itself.
+	// This is done because the formula may reference other fields in the data
+	// object, and they all need to be present when running the object through
+	// temen.
+	keys.forEach(formula => {
+		const fullPath = formula.path;
+		const formulaText = formula.formula;
+
+		if (!fullPath.includes('.items.')) {
+			set(data, fullPath, formulaText);
+		} else {
+			const pathFragments = fullPath.split('.items.');
+			replaceArrayWithFormulaContent(pathFragments, data, formulaText);
+		}
+	});
+};
+
+const replaceArrayWithFormulaContent = (
+	pathFragments: string[],
+	data: any,
+	formulaText: string,
+) => {
+	const firstFragment = pathFragments[0];
+	const lastFragment = pathFragments[1];
+	const array = data[firstFragment] || [];
+	if (pathFragments.length > 2) {
+		const subFragments = pathFragments.slice(1, pathFragments.length);
+		array.forEach((item: any) => {
+			replaceArrayWithFormulaContent(subFragments, item, formulaText);
+		});
+	} else {
+		array.forEach((item: any) => {
+			set(item, lastFragment, formulaText);
+		});
+	}
+};
+
+const getFormulaKeys = (obj: any, prefix: string = '') => {
+	const keys = Object.keys(obj);
+	prefix = prefix ? prefix + '.' : '';
+	return keys.reduce(
+		(result, key) => {
+			if (isPlainObject(obj[key])) {
+				result = result.concat(getFormulaKeys(obj[key], prefix + key));
+			} else if (key === '$$formula') {
+				result.push(prefix + key);
+			}
+			return result;
+		},
+		[] as string[],
+	);
 };
 
 export { defaultValueForSchema, runFormulas };
