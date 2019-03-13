8b51dd340165a86ab0b7e6e3dbbba3ff23121fcb
Jakub Mikulas <jakub@balena.io>
2019-03-13 17:03:52 +0100

    refactor: use date.toISOString() to normalize dates
    Change-type: patch
    Signed-off-by: Jakub Mikulas <jakub@balena.io>

--- src/components/DataTypes/date-time-helpers.tsx
+++ src/components/DataTypes/date-time-helpers.tsx
@@ -1,24 +1,17 @@
 import memoize = require('lodash/memoize');
 
-const pad = (n: number) => (n < 10 ? `0${n}` : n);
-
 export const getDefaultDate = (): string => {
-	const now = new Date();
-	const day = pad(now.getDate());
-	const month = pad(now.getMonth() + 1);
-	return `${now.getFullYear()}-${month}-${day}T10:00`;
+	const date = new Date();
+	date.setUTCHours(0, 0, 0, 0);
+	return date.toISOString().split('.')[0] + 'Z';
 };
 
-// Normalize a timestamp to a RFC3339 timestamp, which is required for JSON
-// schema.
+// Normalize a timestamp to a RFC3339 timestamp, which is required for JSON schema.
 export const normalizeDateTime = memoize((timestamp: string) => {
 	const d = new Date(timestamp);
 	if (isNaN(d.getTime())) {
 		return 'Invalid date';
 	}
-	return `${d.getUTCFullYear()}-${pad(d.getUTCMonth() + 1)}-${pad(
-		d.getUTCDate(),
-	)}T${pad(d.getUTCHours())}:${pad(d.getUTCMinutes())}:${pad(
-		d.getUTCSeconds(),
-	)}Z`;
+	// Remove miliseconds
+	return d.toISOString().split('.')[0] + 'Z';
 });
