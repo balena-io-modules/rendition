aeb97dfc33857841f94de401b07108fc4bb89d32
Stevche Radevski <sradevski@live.com>
2019-09-10 18:04:38 +0200

    Update the divider based on updated styleguide
    Change-type: minor
    Signed-off-by: Stevche Radevski <stevche@balena.io>

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -1,19 +1,59 @@
+import * as React from 'react';
 import styled from 'styled-components';
 import asRendition from '../../asRendition';
 import { DefaultProps, RenditionSystemProps } from '../../common-types';
 import { px } from '../../utils';
+import { Flex } from '../Flex';
+import Txt from '../Txt';
 
 const Base = styled.hr<InternalDividerProps>`
-	border: none;
-	height: ${props => px(props.height || 2)};
-	background-color: ${props =>
-		props.color || props.theme.colors.tertiary.semilight};
+	border: ${props =>
+		`${px(props.height || 0.5)} ${props.type || 'solid'} ${props.color ||
+			props.theme.colors.quartenary.main}}`};
+	height: ${props => px(props.height || 0.5)};
+`;
+
+const FlexBase = styled(Base)`
+	flex: 1 1 0;
+	margin: auto;
 `;
 
 export interface InternalDividerProps extends DefaultProps {
 	height?: number;
 	color?: string;
+	children?: string;
+	type?: 'solid' | 'dashed';
 }
+
+const Divider = React.forwardRef(
+	({ children, className, ...otherProps }: InternalDividerProps, ref) => {
+		if (!children) {
+			return <Base className={className} {...otherProps} ref={ref as any} />;
+		}
+
+		if (typeof children !== 'string') {
+			throw new Error(
+				`The child element of the Divider component must be a string, received: ${typeof children}`,
+			);
+		}
+
+		return (
+			<Flex
+				className={className}
+				width="100%"
+				flexDirection="row"
+				justifyContent="center"
+				ref={ref as any}
+			>
+				<FlexBase {...otherProps} />
+				<Txt.span color="tertiary.main" px={3}>
+					{children}
+				</Txt.span>
+				<FlexBase {...otherProps} />
+			</Flex>
+		);
+	},
+);
 
 export type DividerProps = InternalDividerProps & RenditionSystemProps;
 
@@ -21,4 +61,4 @@ export default asRendition<
 	React.ForwardRefExoticComponent<
 		DividerProps & React.RefAttributes<HTMLHRElement>
 	>
->(Base, [], ['color']);
+>(Divider, [], ['color']);
