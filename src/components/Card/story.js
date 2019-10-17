42d53a848f3d4e14bcb712ec8c9b075d224738d1
Dimitrios Lytras <dnlytras@gmail.com>
2019-10-17 17:27:09 +0300

    Update Card component
    Change-type: minor

--- src/components/Card/story.js
+++ src/components/Card/story.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import withReadme from 'storybook-readme/with-readme'
 import { withScreenshot } from 'storybook-chrome-screenshot'
-import { Card, Link, Flex, Box, Button, Provider, TextWithCopy } from '../../'
+import { Card, Link, Flex, Button, Provider, TextWithCopy } from '../../'
 import Readme from './README.md'
 
 const rows = [
@@ -21,27 +21,35 @@ const rows = [
   <div>Lorem Ipsum dolor si amet</div>
 ]
 
-storiesOf('Core/Card', module)
+storiesOf('Next/Card', module)
   .addDecorator(withReadme(Readme))
   .addDecorator(withScreenshot())
   .add('Standard', () => {
     return (
       <Provider>
-        <Box width={350} m={3}>
-          <Card>
+        <Flex flexWrap='wrap' alignItems='flex-start' m={2}>
+          <Card width={350} m={2}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
             scelerisque euismod risus at gravida. Pellentesque a nunc semper,
             ultrices lacus nec, mattis mauris
           </Card>
-        </Box>
+          <Card small width={350} m={2}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+            scelerisque euismod risus at gravida. Pellentesque a nunc semper,
+            ultrices lacus nec, mattis mauris
+          </Card>
+        </Flex>
       </Provider>
     )
   })
+
   .add('With Header', () => {
     return (
       <Provider>
-        <Box width={350} m={3}>
+        <Flex flexWrap='wrap' alignItems='flex-start' m={2}>
           <Card
+            width={350}
+            m={2}
             title='Card with Button'
             cta={
               <Button
@@ -58,7 +66,27 @@ storiesOf('Core/Card', module)
             ultrices lacus nec, mattis mauris
           </Card>
           <Card
-            mt={3}
+            width={350}
+            m={2}
+            small
+            title='Card with Button'
+            cta={
+              <Button
+                plaintext
+                primary
+                onClick={() => window.alert('Action with Button')}
+              >
+                Update
+              </Button>
+            }
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+            scelerisque euismod risus at gravida. Pellentesque a nunc semper,
+            ultrices lacus nec, mattis mauris
+          </Card>
+          <Card
+            width={350}
+            m={2}
             title='Card with Link'
             cta={<Link href='https://balena.io'>Link</Link>}
           >
@@ -66,37 +94,42 @@ storiesOf('Core/Card', module)
             scelerisque euismod risus at gravida. Pellentesque a nunc semper,
             ultrices lacus nec, mattis mauris
           </Card>
-        </Box>
-      </Provider>
-    )
-  })
-  .add('With rows', () => {
-    return (
-      <Provider>
-        <Box width={350} m={3}>
+
           <Card
-            title='Title'
-            rows={rows}
-            cta={<Link href='https://balena.io'>Link</Link>}
-          />
-        </Box>
-      </Provider>
-    )
-  })
-  .add('Title', () => {
-    return (
-      <Provider>
-        <Box width={350} m={3}>
-          <Card
-            title='Title'
-            minHeight={'300px'}
+            width={350}
+            small
+            m={2}
+            title='Card with Link'
             cta={<Link href='https://balena.io'>Link</Link>}
           >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
             scelerisque euismod risus at gravida. Pellentesque a nunc semper,
             ultrices lacus nec, mattis mauris
           </Card>
-        </Box>
+        </Flex>
+      </Provider>
+    )
+  })
+  .add('With rows', () => {
+    return (
+      <Provider>
+        <Flex flexWrap='wrap' alignItems='flex-start' m={2}>
+          <Card
+            m={2}
+            width={350}
+            title='Title'
+            rows={rows}
+            cta={<Link href='https://balena.io'>Link</Link>}
+          />
+          <Card
+            m={2}
+            width={350}
+            small
+            title='Title'
+            rows={rows}
+            cta={<Link href='https://balena.io'>Link</Link>}
+          />
+        </Flex>
       </Provider>
     )
   })
