c85f61e44b21e3b7ba23f3389d7dc0b7b1e55e84
Amit Solanki <amit.fash@gmail.com>
2020-03-14 11:42:52 +0530

    Lock Grommet version to 2.11.1.
    v2.11.2 breaks typescript build.
    
    Change-type: Minor
    Signed-off-by: Amit Solanki <amit@balena.io>

--- src/components/Notifications/defaultStyle.ts
+++ src/components/Notifications/defaultStyle.ts
@@ -7,6 +7,7 @@ export default css`
 	.notification-container-bottom-center,
 	.notification-container-bottom-left,
 	.notification-container-bottom-right,
+	.notification-container-center,
 	.notification-container-top-center,
 	.notification-container-top-left,
 	.notification-container-top-right {
@@ -14,15 +15,33 @@ export default css`
 		position: absolute;
 		pointer-events: all;
 	}
+	.notification-container-bottom-center,
+	.notification-container-center,
 	.notification-container-top-center {
-		transform: translateX(-50%);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		left: calc(50% - 162.5px);
+	}
+	.notification-container-center {
 		top: 20px;
-		left: 50%;
+		height: 100%;
+		pointer-events: none;
+	}
+	.notification-container-center .center-inner {
+		width: 325px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		pointer-events: all;
+	}
+	.notification-container-top-center {
+		top: 20px;
 	}
 	.notification-container-bottom-center {
-		transform: translateX(-50%);
 		bottom: 20px;
-		left: 50%;
 	}
 	.notification-container-top-left {
 		left: 20px;
@@ -205,26 +224,23 @@ export default css`
 		left: 50%;
 		top: 50%;
 	}
-	.n-parent {
-		width: 275px;
-	}
-	.notification-container-mobile-bottom .n-parent,
 	.notification-container-mobile-bottom .notification-item,
-	.notification-container-mobile-top .n-parent,
-	.notification-container-mobile-top .notification-item {
+	.notification-container-mobile-bottom .notification-parent,
+	.notification-container-mobile-top .notification-item,
+	.notification-container-mobile-top .notification-parent {
 		max-width: 100%;
 		width: 100%;
 	}
-	.notification-container-bottom-right .n-parent,
-	.notification-container-top-right .n-parent {
+	.notification-container-bottom-right .notification-parent,
+	.notification-container-top-right .notification-parent {
 		margin-left: auto;
 	}
-	.notification-container-bottom-left .n-parent,
-	.notification-container-top-left .n-parent {
+	.notification-container-bottom-left .notification-parent,
+	.notification-container-top-left .notification-parent {
 		margin-right: auto;
 	}
-	.notification-container-mobile-bottom .n-parent,
-	.notification-container-mobile-top .n-parent {
+	.notification-container-mobile-bottom .notification-parent,
+	.notification-container-mobile-top .notification-parent {
 		margin-left: auto;
 		margin-right: auto;
 	}
