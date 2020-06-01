ba8d20e4bc20ac66e3866e459b43bbf692aac52d
StefKors <stef.kors@gmail.com>
2020-06-01 10:48:18 +0200

    Add support for scrollable tabs
    Change-type: patch
    Signed-off-by: Stef Kors <stef@balena.io>

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,10 +1,82 @@
-import { Tabs as GrommetTabs, TabsProps } from 'grommet';
-export { Tab, TabProps, TabsProps } from 'grommet';
+import styled from 'styled-components';
+import {
+	TabProps,
+	Tab as GrommetTab,
+	Tabs as GrommetTabs,
+	TabsProps,
+} from 'grommet';
+export { TabProps, TabsProps } from 'grommet';
 import * as React from 'react';
 import asRendition from '../../asRendition';
 
-const TabsBase = ({ ...props }: TabsProps) => {
-	return <GrommetTabs justify="start" {...props} />;
+interface InnerTabProps extends TabProps {
+	compact?: boolean;
+}
+
+interface InnerTabsProps extends TabsProps {
+	compact?: boolean;
+}
+
+const ScrollWrapper = styled(GrommetTabs)<{
+	compact?: boolean;
+}>`
+	${(props) => {
+		if (props.compact) {
+			return `
+				& > div {
+					scroll-snap-type: x mandatory;
+					overflow-x: scroll;
+					display: flex;
+					flex-wrap: nowrap;
+
+					scrollbar-width: none;
+					-ms-overflow-style: none;
+
+					&::-webkit-scrollbar {
+						width: 0px;
+					}
+				}
+			`;
+		}
+	}}
+`;
+
+const TabTitle = styled(GrommetTab)<{
+	compact?: boolean;
+}>`
+	${(props) => {
+		if (props.compact) {
+			return `
+			& > div {
+				white-space: nowrap;
+				overflow: hidden;
+				text-overflow: ellipsis;
+				scroll-snap-align: center;
+			}
+			`;
+		}
+	}}
+`;
+
+export const Tab = ({ compact, title, ...props }: InnerTabProps) => {
+	return (
+		<TabTitle compact={compact} title={title} {...props}>
+			{title}
+		</TabTitle>
+	);
+};
+
+const TabsBase = ({ children, ...props }: InnerTabsProps) => {
+	return (
+		<ScrollWrapper justify="start" {...props}>
+			{React.Children.map(
+				children,
+				(tab: React.ReactElement<InnerTabProps>) => {
+					return React.cloneElement(tab, { compact: props.compact });
+				},
+			)}
+		</ScrollWrapper>
+	);
 };
 
 export const Tabs = (asRendition(
--- src/components/Tabs/story.js
+++ src/components/Tabs/story.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import withReadme from 'storybook-readme/with-readme'
-import { Tab, Tabs, Txt } from '../../'
+import { Tab, Tabs, Txt, Box } from '../../'
 import Readme from './README.md'
 
 storiesOf('Next/Tabs', module)
@@ -34,5 +34,22 @@ storiesOf('Next/Tabs', module)
           <Txt mt={3}>Here is tab #3</Txt>
         </Tab>
       </Tabs>
+    )
+  })
+  .add('Compact', () => {
+    return (
+      <Box maxWidth={400}>
+        <Tabs p={3} compact>
+          <Tab title='This is a very long tab name'>
+            <Txt mt={3}>Here is tab #1</Txt>
+          </Tab>
+          <Tab title='This is another very long tab name'>
+            <Txt mt={3}>Here is tab #2</Txt>
+          </Tab>
+          <Tab title='And this is a third very long tab name'>
+            <Txt mt={3}>Here is tab #3</Txt>
+          </Tab>
+        </Tabs>
+      </Box>
     )
   })
