fd93ae2aa407038d974a5902388558a6fbe5c1f8
StefKors <stef.kors@gmail.com>
2020-06-19 10:02:32 +0200

    WIP
    Change-type: patch
    Signed-off-by: Stef Kors <stef@balena.io>

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -8,9 +8,12 @@ import {
 export { TabProps, TabsProps } from 'grommet';
 import * as React from 'react';
 import asRendition from '../../asRendition';
+import { Flex } from '../Flex';
 
 interface InnerTabProps extends TabProps {
 	compact?: boolean;
+	index: number;
+	length?: any;
 }
 
 interface InnerTabsProps extends TabsProps {
@@ -58,21 +61,39 @@ const TabTitle = styled(GrommetTab)<{
 	}}
 `;
 
-export const Tab = ({ compact, title, ...props }: InnerTabProps) => {
+export const Tab = ({
+	compact,
+	title,
+	index,
+	length,
+	...props
+}: InnerTabProps) => {
+	console.log(index, length);
+
 	return (
-		<TabTitle compact={compact} title={title} {...props}>
-			{title}
-		</TabTitle>
+		<Flex>
+			{index === 0 && <button>left</button>}
+			<TabTitle compact={compact} title={title} {...props}>
+				{title}
+			</TabTitle>
+			{index + 1 === length && <button>right</button>}
+		</Flex>
 	);
 };
 
 const TabsBase = ({ children, ...props }: InnerTabsProps) => {
+	const mapLength = React.Children.count(children);
+
 	return (
 		<ScrollWrapper justify="start" {...props}>
 			{React.Children.map(
 				children,
-				(tab: React.ReactElement<InnerTabProps>) => {
-					return React.cloneElement(tab, { compact: props.compact });
+				(tab: React.ReactElement<InnerTabProps>, index: number) => {
+					return React.cloneElement(tab, {
+						compact: props.compact,
+						index,
+						length: mapLength,
+					});
 				},
 			)}
 		</ScrollWrapper>
