9285abc9f63c1cc11e5d32b1ec2709be5a24989f
Graham McCulloch <admin@grahammcculloch.co.uk>
2020-08-06 15:37:08 +0700

    Add ErrorBoundary internal component
    Change-type: patch
    Signed-off-by: Graham McCulloch <graham@balena.io>

---
+++ src/internal/ErrorBoundary/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Flex } from '../../components/Flex';
+import Alert from '../../components/Alert';
+
+interface ErrorBoundaryState {
+	error: any;
+}
+
+interface ErrorBoundaryProps {
+	getErrorDisplay?: (error: any) => JSX.Element;
+}
+
+const DefaultErrorDisplay = () => (
+	<Flex flex={1} alignItems="center" justifyContent="center">
+		<Alert m={2} plaintext danger>
+			Something went wrong
+		</Alert>
+	</Flex>
+);
+
+export default class ErrorBoundary extends React.Component<
+	ErrorBoundaryProps,
+	ErrorBoundaryState
+> {
+	constructor(props: ErrorBoundaryProps) {
+		super(props);
+		this.state = { error: null };
+	}
+
+	static getDerivedStateFromError(error: any) {
+		return { error };
+	}
+
+	render() {
+		if (this.state.error) {
+			return this.props.getErrorDisplay ? (
+				this.props.getErrorDisplay(this.state.error)
+			) : (
+				<DefaultErrorDisplay />
+			);
+		}
+
+		return this.props.children;
+	}
+}
