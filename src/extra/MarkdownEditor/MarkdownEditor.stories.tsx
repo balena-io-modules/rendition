97dc3fbc1545d5bb3f0054af84876b409b23cc52
karaxuna <kaxabernet@gmail.com>
2021-09-14 11:05:55 +0000

    Allow uploading files
    Change-type: minor

--- src/extra/MarkdownEditor/MarkdownEditor.stories.tsx
+++ src/extra/MarkdownEditor/MarkdownEditor.stories.tsx
@@ -12,3 +12,11 @@ const Template = createTemplate<MarkdownEditorProps>(MarkdownEditor);
 export const Default = createStory<MarkdownEditorProps>(Template, {
 	value: source,
 });
+
+export const WithFileUpload = createStory<MarkdownEditorProps>(Template, {
+	value: source,
+	fileUploadFunction: async (file: File) => {
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+		return `/${file.name}`;
+	},
+});
