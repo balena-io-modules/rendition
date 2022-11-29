835c82eb4597b322961a68820899f22be35051c8
Edwin Joassart <edwin@blck.coffee>
2022-11-29 13:54:45 +0100

    minor: add .etch preloading option to download modal

--- src/hooks/useTranslation.tsx
+++ src/hooks/useTranslation.tsx
@@ -86,6 +86,8 @@ const translationMap = {
 	'actions.download_balenaos': 'Download balenaOS',
 	'actions.download_configuration_file_only':
 		'Download configuration file only',
+	'actions.download_dot_etch':
+		'Download preloaded .etch',
 	'actions.flash': 'Flash',
 
 	'warning.etcher_min_requirement': 'Etcher v1.7.2 or greater is required',
